551efe15be8abc507fa2be31d7671331a64dc151
Maria <belenzar90@gmail.com>
2023-07-19 13:55:05 -0300

    🔧 [Config]: Install React Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import React, { useState } from 'react'
+import React from 'react'
 import MainHeader from './Components/MainHeader.jsx'
-
+import {Routes, Route} from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
     <>
-     <MainHeader></MainHeader>
-    </>
+
+    <MainHeader>
+   
+    </MainHeader>
+  </>
   )
 }
 
