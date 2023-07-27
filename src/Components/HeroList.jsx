b76c17a39a162500fee801af6bb39c59e41b40f9
Jose Sulbaran <twjasa@gmail.com>
2023-07-27 13:29:52 -0400

    feature: improvements + show modal with comics

--- src/Components/HeroList.jsx
+++ src/Components/HeroList.jsx
@@ -1,33 +1,43 @@
 import React from "react";
+import { createPortal } from "react-dom";
 import { useNavigate } from "react-router-dom";
-import HeroCard from "./HeroCard.jsx";
-import './HeroList.css';
+import "./HeroList.css";
+import { Modal } from "./Modal/Modal.jsx";
 import NotFound from "./NotFound.jsx";
 
-export const HeroList =({data}) => {
-    let navigate = useNavigate();
+export const HeroList = ({ data }) => {
+  const [modal, setModal] = React.useState({ show: false, data: {}, heroId: "" });
+  let navigate = useNavigate();
+  return (
+    <>
+      {data ? (
+        data.map((hero) => {
+          return (
+            <div
+              className="hero-card"
+              key={hero.id}
+              onClick={() => setModal({ show: true, data: {}, heroId: hero.id })}
+            >
+              <img
+                src={`${hero.thumbnail.path}.${hero.thumbnail.extension}`}
+                alt="hero picture"
+              />
+              <div className="title">
+                <h2>{hero.name}</h2>
+              </div>
+            </div>
+          );
+        })
+      ) : (
+        <NotFound />
+      )}
+      {modal.show &&
+        createPortal(
+          <Modal onClose={() => setModal({ show: false, data: {} })} body={modal.data} heroId={modal.heroId}/>,
+          document.body,
+        )}
+    </>
+  );
+};
 
-    return (
-        <>
-        {
-        (data)?(
-            data.map(hero => {
-                return(
-                <div className="hero-card" key={hero.id} onClick={()=>navigate(`/${hero.id}`)}>
-                    <img src={`${hero.thumbnail.path}.${hero.thumbnail.extension}`} alt="hero picture"></img>
-                    <div className="title">
-                    <h2>{hero.name}</h2>
-                    </div>
-                    <HeroCard></HeroCard>
-                </div>
-                )
-            })
-        ) : <NotFound/>
-    }
-        </>
-    )
-
-}
-
-export default HeroList
-
+export default HeroList;
---
+++ src/Components/Loader/Loader.jsx
@@ -0,0 +1,12 @@
+import "./loader.css";
+
+export function Loader() {
+  return (
+    <div className="lds-ring">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
